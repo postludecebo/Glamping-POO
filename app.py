b7e376be632afca5408d2bc895ee34528dd6af61
postludecebo <seleon56@soy.sena.edu.co>
2025-06-15 03:40:30 -0500

    Se crea el nav bar, las páginas para el formulario de cada clase (Servicio Adicional, Empleado, Glamping, Huésped, Alojamiento, Persona, Reserva, y se crea un formulario plantilla del formulario Persona en cada página.

--- app.py
+++ app.py
@@ -6,11 +6,12 @@ app = Flask(__name__)
 
 personArray = []
 
+
 @app.route("/")
 def initiate():
     return render_template("UI.html")
 
-@app.route("/process", methods=["POST"])
+@app.route("/processPerson", methods=["POST"])
 def process():
     name = request.form.get("name")
     phone = request.form.get("phone")
@@ -27,11 +28,37 @@ def process():
     })
     return render_template("listRegister.html", persons=personArray)
     
-@app.route("/registers")
+@app.route("/registersPersons")
 def show_registers():
     return render_template("listRegister.html", persons=personArray)
 
+@app.route('/create-additional-service')
+def create_additional_service():
+    return render_template('createAdditionalService.html')
 
+@app.route('/create-employee')
+def create_employee():
+    return render_template('createEmployee.html')
+
+@app.route('/create-guest')
+def create_guest():
+    return render_template('createGuest.html')
+
+@app.route('/create-hosting')
+def create_hosting():
+    return render_template('createHosting.html')
+
+@app.route('/create-person')
+def create_person():
+    return render_template('createPerson.html')
+
+@app.route('/create-reservation')
+def create_reservation():
+    return render_template('createReservation.html')
+
+@app.route('/create-glamping')
+def create_glamping():
+    return render_template('createGlamping.html')
 
 if __name__ == "__main__":
     app.run(debug=True)
